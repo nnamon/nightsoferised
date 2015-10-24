95ae72602c798c687739c7a1a7c13520f68336ae
amon <jergorn93@gmail.com>
2015-10-24 20:11:02 +0800

    Verve

--- sessions/session2/gdbpractice/verve/verve.c
+++ sessions/session2/gdbpractice/verve/verve.c
@@ -16,7 +16,7 @@ char* symphony() {
 int main(int argc, char ** argv) {
     char * message = "No!";
     if (strcmp(argv[1], bitter()) == 0) {
-        if (strcmp(argv[2], sweet() == 0)) {
+        if (strcmp(argv[2], sweet()) == 0) {
             if (strcmp(argv[3], symphony()) == 0) {
                 message = "Yes!";
             }
