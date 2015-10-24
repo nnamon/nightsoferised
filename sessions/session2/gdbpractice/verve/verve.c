ed21fb45c5e1a6a837b42f0d5219ab94812a2fd5
amon <jergorn93@gmail.com>
2015-10-24 20:08:09 +0800

    Verve

---
+++ sessions/session2/gdbpractice/verve/verve.c
@@ -0,0 +1,26 @@
+int main(int argc, char ** argv) {
+    char * message = "No!";
+    if (strcmp(argv[1], bitter()) == 0) {
+        if (strcmp(argv[2], sweet() == 0)) {
+            if (strcmp(argv[3], symphony()) == 0) {
+                message = "Yes!";
+            }
+        }
+    }
+    printf(message);
+}
+
+char* bitter() {
+    char *b = "bitter";
+    return b;
+}
+
+char* sweet() {
+    char *b = "sweet";
+    return b;
+}
+
+char* symphony() {
+    char *b = "symphony";
+    return b;
+}
